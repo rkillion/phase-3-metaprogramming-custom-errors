e3eec746106ee34b4a9bbe18106d3cd6d48b7ca9
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-01 12:25:35 -0400

    commit tree 791fee1fa62cabc067ed5e226c44da75eb1113b4

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,13 +1,51 @@
+require "pry"
+
 class Person
   attr_accessor :partner, :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.get_by_name(name)
+    self.all.find{|p|p.name==name}
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      person.partner = self
+      self.partner = person
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+        # input = error.message(person)
+        # if input=="y"
+        #   new_partner = Person.new(person)
+        #   self.partner = new_partner
+        #   new_partner.partner = self
+        #   puts "#{self.name} and #{new_partner.name} have been married!"
+        # else
+        #   puts "Marriage cancelled."
+        # end
+      end
+    end
+  end
+
+  class PartnerError < StandardError
+    def message #(person)
+      "you must give the get_married method an argument of an instance of the person class!"
+      # print "Would you like to make #{person} a Person? y/n: "
+      # input = gets.chomp
+      # input
+    end
   end
 
 end
@@ -16,6 +54,6 @@ beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-
+binding.pry
 
 
